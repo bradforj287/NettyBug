8f0376594eb159b76f873d1fec1cec9f6036f59e
Brad Jones <brjones@akamai.com>
2018-04-09 11:28:18 -0400

    refactor

---
+++ src/main/java/com/bradforj287/nettybug/handlers/CustomHttpProxyHandler2.java
@@ -0,0 +1,109 @@
+package com.bradforj287.nettybug.handlers;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+public class CustomHttpProxyHandler2 extends SimpleChannelInboundHandler<HttpObject> {
+
+    private final String host;
+    private final int port;
+    private final SslContext sslContext;
+
+    private Channel outboundChannel;
+
+    public CustomHttpProxyHandler2(String host, int port) {
+        this.host = host;
+        this.port = port;
+        try {
+            this.sslContext = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        buildOutboundConnection(ctx).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (future.isSuccess()) {
+                    outboundChannel = future.channel();
+                    ctx.channel().config().setOption(ChannelOption.AUTO_READ, true);
+                    ctx.read();
+                } else {
+                    ctx.close();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        boolean isWritable = ctx.channel().isWritable();
+        outboundChannel.config().setOption(ChannelOption.AUTO_READ, isWritable);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        outboundChannel.close();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        outboundChannel.writeAndFlush(msg, outboundChannel.voidPromise());
+    }
+
+    private ChannelFuture buildOutboundConnection(ChannelHandlerContext writeChanel) {
+        SimpleChannelInboundHandler<HttpObject> backend = new SimpleChannelInboundHandler<HttpObject>() {
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                ctx.read();
+            }
+
+            @Override
+            public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+                boolean isWritable = ctx.channel().isWritable();
+                writeChanel.channel().config().setOption(ChannelOption.AUTO_READ, isWritable);
+            }
+
+            @Override
+            public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                writeChanel.close();
+            }
+
+            @Override
+            protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+                if (msg instanceof HttpContent) {
+                    ((HttpContent) msg).retain();
+                }
+                writeChanel.writeAndFlush(msg, writeChanel.voidPromise());
+            }
+        };
+
+        Bootstrap b = new Bootstrap();
+        b.group(writeChanel.channel().eventLoop())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        if (port == 443) {
+                            p.addLast(sslContext.newHandler(ch.alloc()));
+                        }
+                        p.addLast(new HttpClientCodec());
+                        p.addLast(backend);
+                    }
+                }).option(ChannelOption.AUTO_READ, true);
+
+        return b.connect(host, port);
+    }
+}
---
+++ src/main/java/com/bradforj287/nettybug/handlers/InternalProxyHandler2.java
@@ -0,0 +1,82 @@
+package com.bradforj287.nettybug.handlers;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.local.LocalAddress;
+import io.netty.channel.local.LocalChannel;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpObject;
+
+public class InternalProxyHandler2 extends SimpleChannelInboundHandler<HttpObject> {
+    private Channel outboundChannel;
+    private final boolean autoRead;
+
+    public InternalProxyHandler2(boolean autoRead) {
+        this.autoRead = autoRead;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        buildOutboundConnection(ctx).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (future.isSuccess()) {
+                    outboundChannel = future.channel();
+                    ctx.channel().config().setOption(ChannelOption.AUTO_READ, autoRead);
+                    ctx.read();
+                } else {
+                    ctx.close();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        outboundChannel.close();
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        NettyUtils.flushToChannel(outboundChannel, ctx.channel(), msg);
+    }
+
+    private ChannelFuture buildOutboundConnection(ChannelHandlerContext writeChanel) {
+        SimpleChannelInboundHandler<HttpObject> backend = new SimpleChannelInboundHandler<HttpObject>() {
+            @Override
+            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                ctx.read();
+            }
+
+            @Override
+            public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                writeChanel.close();
+            }
+
+            @Override
+            protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+                if (msg instanceof HttpContent) {
+                    ((HttpContent) msg).retain();
+                }
+                NettyUtils.flushToChannel(writeChanel.channel(), ctx.channel(), msg);
+            }
+        };
+
+        final LocalAddress addr = new LocalAddress("internalServer");
+
+        Bootstrap b = new Bootstrap();
+        b.group(writeChanel.channel().eventLoop())
+                .channel(LocalChannel.class)
+                .handler(new ChannelInitializer<LocalChannel>() {
+                    @Override
+                    protected void initChannel(LocalChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        p.addLast(new HttpClientCodec());
+                        p.addLast(backend);
+                    }
+                }).option(ChannelOption.AUTO_READ, autoRead);
+
+        return b.connect(addr);
+    }
+}
