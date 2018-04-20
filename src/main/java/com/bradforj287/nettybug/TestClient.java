ac6d7a55d150c4344c105366d0714f842ef2f542
Brad Jones <brjones@akamai.com>
2018-04-20 18:10:42 -0400

    code

--- src/main/java/com/bradforj287/nettybug/TestClient.java
+++ src/main/java/com/bradforj287/nettybug/TestClient.java
@@ -8,13 +8,16 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TestClient {
     private static Logger logger = LoggerFactory.getLogger(ServerMainBroken.class);
 
-    private final static String URL = "<ENTER URL HERE!!!>";
-    private final static int iterations = 100000;
+    private final static String URL = "<ENTER URL HERE>";
+    private final static int NUM_THREADS = 10;
+    private final static int iterations = 2000;
     private final static boolean validateAllSameResponse = false;
 
     private static String getHTML(String urlToRead) throws Exception {
@@ -34,22 +37,48 @@ public class TestClient {
     public static void main(String args[]) throws Exception {
         logger.info(String.format("Hitting URL: %s", URL));
         logger.info(String.format("Executing %s iterations", iterations));
-        Stopwatch sw = Stopwatch.createStarted();
 
+        List<Thread> tlist = new ArrayList<>();
+
+        Stopwatch sw = Stopwatch.createStarted();
+        for (int i = 0; i < NUM_THREADS; i++) {
+            final int threadNum = i;
+            Thread t = new Thread(() -> {
+                try {
+                    doTest(threadNum);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            tlist.add(t);
+            t.start();
+        }
+
+        tlist.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        double seconds = sw.elapsed(TimeUnit.SECONDS);
+        double ips = iterations * NUM_THREADS / seconds;
+
+        logger.info(String.format("Requests per second = %s", ips));
+    }
+
+    private static void doTest(int threadNum) throws Exception {
         final String first = getHTML(URL);
         logger.info("Finished iteration 0");
         for (int i = 1; i < iterations; i++) {
             final String str = getHTML(URL);
-            logger.info("finished iteration " + i);
+            logger.info(String.format("tNum = %s finished iteration %s", threadNum, i));
             if (validateAllSameResponse && !str.equals(first)) {
                 logger.info("Values do not match, exiting");
                 break;
             }
         }
-
-        double seconds = sw.elapsed(TimeUnit.SECONDS);
-        double ips = iterations / seconds;
-
-        logger.info(String.format("Requests per second = %s", ips));
     }
 }
